9f2ecda98d3531686a1dbca5b333bc441a2269a9
Steven G. Harms <steven.harms@gmail.com>
2014-07-16 11:29:14 -0700

    Correct is-copied QuerentMutators::Irregular file

--- lib/latinverb_querent_for_classification_builder/querent_mutators/irregular.rb
+++ lib/latinverb_querent_for_classification_builder/querent_mutators/irregular.rb
@@ -1,58 +1,35 @@
+#encoding: UTF-8
 
 module Linguistics
   module Latin
     module Verb
       class LatinVerb
-        class IrregularQuerent < Querent
-          TENSE_METHOD_DEFINITIONS = [
-            IrregularQuerent::ActiveVoiceImperativeMoodFutureTenseMethods,
-            IrregularQuerent::ActiveVoiceImperativeMoodPresentTenseMethods,
-            IrregularQuerent::ActiveVoiceIndicativeMoodFutureTenseMethods,
-            IrregularQuerent::ActiveVoiceIndicativeMoodFutureperfectTenseMethods,
-            IrregularQuerent::ActiveVoiceIndicativeMoodImperfectTenseMethods,
-            IrregularQuerent::ActiveVoiceIndicativeMoodPastperfectTenseMethods,
-            IrregularQuerent::ActiveVoiceIndicativeMoodPerfectTenseMethods,
-            IrregularQuerent::ActiveVoiceIndicativeMoodPresentTenseMethods,
-            IrregularQuerent::ActiveVoiceSubjunctiveMoodImperfectTenseMethods,
-            IrregularQuerent::ActiveVoiceSubjunctiveMoodPastperfectTenseMethods,
-            IrregularQuerent::ActiveVoiceSubjunctiveMoodPerfectTenseMethods,
-            IrregularQuerent::ActiveVoiceSubjunctiveMoodPresentTenseMethods,
-            IrregularQuerent::PassiveVoiceIndicativeMoodFutureTenseMethods,
-            IrregularQuerent::PassiveVoiceIndicativeMoodFutureperfectTenseMethods,
-            IrregularQuerent::PassiveVoiceIndicativeMoodImperfectTenseMethods,
-            IrregularQuerent::PassiveVoiceIndicativeMoodPastperfectTenseMethods,
-            IrregularQuerent::PassiveVoiceIndicativeMoodPerfectTenseMethods,
-            IrregularQuerent::PassiveVoiceIndicativeMoodPresentTenseMethods,
-            IrregularQuerent::PassiveVoiceIndicativeMoodPresentTenseMethods,
-            IrregularQuerent::PassiveVoiceSubjunctiveMoodImperfectTenseMethods,
-            IrregularQuerent::PassiveVoiceSubjunctiveMoodPastperfectTenseMethods,
-            IrregularQuerent::PassiveVoiceSubjunctiveMoodPerfectTenseMethods,
-            IrregularQuerent::PassiveVoiceSubjunctiveMoodPresentTenseMethods,
-          ]
+        module QuerentMutators
+          class Irregular
+            attr_reader :querent
 
-          def initialize(deserialized_structure)
-            @structure = OpenStruct.new(deserialized_structure)
-            @added_vectorized_methods = []
+            def initialize(lookup_string, passive_perfect_participle, *args)
+              @structure = JsonDeserializer.new(lookup_string).revivified_data_structure
+              @original_string = Array(args).first
+              @querent = LatinVerb::IrregularQuerent.new(@structure)
+            end
 
-            add_tense_methods!
-          end
+            def imperative_handler
+              raw = @structure["tense_blocks"]
+              source = raw["active_voice_imperative_mood_present_tense"] || raw["active voice_imperative_mood_present_tense"]
+              raw = source["data"]
+              OpenStruct.new( :imperatives => [String(raw[1]).gsub(/\s+/,''), String(raw[4]).gsub(/\s+/,'')] )
+            end
 
-          def add_tense_methods!
-            collection = TENSE_METHOD_DEFINITIONS + tense_definitions_template
-            extend_with_tense_method_definitions!(collection)
-            register_methods!(collection)
-          end
+            def infinitivizer
+              OpenStruct.new(@structure['infinitives']['data'].merge(:participle_methods => @structure['participles']['data'].keys))
+            end
 
-          private
-
-          def tense_blocks
-            @tense_blocks ||= OpenStruct.new(@structure["tense_blocks"])
-          end
-
-          def deserialized_tense_block_accessor(block_name)
-            serialized_tense_block = tense_blocks.send(block_name)["data"]
-            opts = {:meaning => MEANINGS[block_name.to_sym]}
-            TenseBlock.new(serialized_tense_block, opts)
+            def participler
+              participles_data = @structure['participles']['data']
+              supine_data = SupineInferrer.new(participles_data["perfect_passive_participle"]).supine
+              OpenStruct.new(participles_data.merge({:supine => supine_data}))
+            end
           end
         end
       end
